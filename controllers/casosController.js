831eab9890bb535dc614d7feb2ba6f4cf607b450
Pedro Dias <pedroddias98@gmail.com>
2025-07-24 14:54:33 -0300

    Estrutura inicial de casos

---
+++ controllers/casosController.js
@@ -0,0 +1,9 @@
+const casosRepository = require("../repositories/casosRepository");
+function getAllCasos(req, res) {
+  const casos = casosRepository.findAll();
+  res.json(casos);
+}
+
+module.exports = {
+  getAllCasos,
+};
